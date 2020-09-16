edb98e1d14070d5b05ad8147de94a587a2678b59
yanuar456 <moch.yanuar.f@gmail.com>
2020-09-16 12:59:18 +0700

    Menambahkan file commit

---
+++ Biodata.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+	printf("Nama\t\t\t: Mochammad Yanuar Fironi\n");
+	printf("NPM\t\t\t: 19081010036\n");
+	printf("Tempat, tanggal lahir\t: Surabaya, 4 Januari 2001\n");
+	printf("Alamat\t\t\t: Puri Prima sari A3/01 Ketegan Tanggulangin, Sidoarjo\n");
+	printf("Pendidikan saat ini\t: Informatika - UPN 'Veteran' Jawa Timur\n");
+	printf("Pendidikan terakhir\t: Teknik Konstruksi Kayu - SMKN 1 Sidoarjo\n");
+	printf("Jenis kelamin\t\t: Laki-laki\n");
+	
+	return 0;
+}
